5087c45fa357a2517976995fd51aff6ead221cd0
Kay <i@v2bv.net>
2018-04-05 20:41:23 -0700

    Finishing up configuration utility

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,5 @@
+//! Configuration save & load utility
+
 // Parse config using crate toml
 extern crate toml;
 
@@ -11,12 +13,14 @@ use std::io::prelude::*;
 // Bot configuration
 #[derive(Debug)]
 #[derive(PartialEq)]
+#[derive(Serialize)]
 #[derive(Deserialize)]
 pub struct Config {
     pub token: Option<String>,
 }
 
 impl Config {
+    /// Generate new config
     pub fn new(token: &String) -> Result<Config, &'static str> {
         if token.len() > 0 {
             return Ok(
@@ -27,6 +31,24 @@ impl Config {
         } else {
             return Err("Invalid token");
         }
+    }
+
+    /// Write config to file at given location
+    pub fn write(&self, location: &String) -> Result<(), Box<Error>> {
+        let content = toml::to_string(&self);
+        if let Err(e) = content {
+            return Err(Box::new(e));
+        }
+        let content: String = content.unwrap();
+        let buffer = File::create(location);
+        if let Err(e) = buffer {
+            return Err(Box::new(e));
+        }
+        let mut buffer = buffer.unwrap();
+        if let Err(e) = buffer.write(content.as_bytes()) {
+            return Err(Box::new(e));
+        }
+        return Ok(());
     }
 }
 
@@ -68,5 +90,30 @@ mod test {
             );
         }
         fs::remove_file("test.toml").unwrap();
+    }
+
+    // Test config generating functionality
+    #[test]
+    fn config_generate() {
+        assert_eq!(
+            Config::new(&"test".to_string()).unwrap(),
+            Config {
+                token: Some("test".to_string()),
+            }
+        );
+    }
+
+    // Test config writing functionality
+    #[test]
+    fn config_write() {
+        let config = Config::new(&"test".to_string()).unwrap();
+        config.write(&"test.toml".to_string());
+        let mut file = fs::File::open("test.toml").unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+        assert_eq!(
+            content,
+            "token = \"test\"\n".to_string()
+        );
     }
 }
